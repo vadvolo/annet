876867c0cc3f2b0db19c3a8316e2ef19a347b02c
Vadim Volovik <vdvolovik@yandex-team.ru>
2025-06-27 13:40:28 +0200

    update

---
+++ annet/__main__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Entry point for running annet as a module with python -m annet
+"""
+
+import sys
+from annet.annet import main
+
+if __name__ == "__main__":
+    sys.exit(main())
